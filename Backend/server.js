01ea6b849452e8276dd87769cc3dfae66631722b
unknown <junejav12@gmail.com>
2026-04-24 15:54:46 +0530

    mongo atlas fix

--- Backend/server.js
+++ Backend/server.js
@@ -6,9 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/hotelDB")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
+
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/customers", require("./routes/customerRoutes"));
